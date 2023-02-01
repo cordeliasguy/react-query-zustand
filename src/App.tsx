598511b4890ffa225654bdcee6a995337605e163
Daniel A. Caballero <danielalejandrocc33@gmail.com>
2023-02-01 12:43:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useFetchRepositories } from './hooks/useRepos'
+import Card from './components/Card'
+import { useFavoriteReposStore } from './store/favoriteRepos'
+
+function App() {
+  const { data, isLoading } = useFetchRepositories('cordeliasguy')
+  const { favoriteReposIds } = useFavoriteReposStore()
+
+  if (isLoading) return <div>Loading...</div>
+
+  console.log(data)
+
+  return (
+    <div>
+      {data?.map(repository => (
+        <Card
+          key={repository.id}
+          repository={repository}
+          isFavorite={favoriteReposIds.includes(repository.id)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default App
